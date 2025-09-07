5eb237687be973b2f4783c2e3b0c2a1c8b1cd519
day <s419922572@gmail.com>
2025-09-07 13:07:18 +0900

    Refactor `abc421/a.py` for input handling and simplify return logic

--- abc421/a.py
+++ abc421/a.py
@@ -1,22 +1,12 @@
 def solve():
     # Step 1: input n, s[], x, y
-    n = input()
-    s = []
-    for i in range(int(n)):
-        s.append(input())
-    # print(s)
-    x, y = map(str, input().split(' '))
+    n = int(input())
+    s = [input() for _ in range(n)]
+    x, y = input().split()
     x = int(x)
 
     # check
-    result = False
-
-    if s[x - 1] == y:
-        result = True
-    if result is True:
-        return 'Yes'
-    else:
-        return 'No'
+    return 'Yes' if s[x - 1] == y else 'No'
 
 # solve()
 print(solve())
